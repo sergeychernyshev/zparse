d71ef8de751e07498f452d63bf35f4f16240770f
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-04-13 22:40:16 -0300

    Update cgi-bin/zparse/zparseparams.pl

--- cgi-bin/zparse/zparseparams.pl
+++ cgi-bin/zparse/zparseparams.pl
@@ -1,3 +1,3 @@
 $zparse2::DEBUG=255;
-$zparse2::DOCUMENT_ROOT="/home/alex.program.ru/www";
+$zparse2::DOCUMENT_ROOT="/home/example.ru/www";
 $zparse2::DATA_FILES_DIR="$zparse2::DOCUMENT_ROOT/datafiles";
